941d4aa81d56737a47e4c92cd03fc9ee69669230
elede79 <69643646+elede79@users.noreply.github.com>
2021-07-21 19:14:42 -0300

    inicial commit

---
+++ src/main/java/com/example/simplewebapplication/SimpleWebApplication.java
@@ -0,0 +1,15 @@
+package com.example.simplewebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SimpleWebApplication.class, args);
+		//System.out.println("Hola mundo");
+	}
+
+}
